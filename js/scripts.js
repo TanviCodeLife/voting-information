500ed742e6b7445540b20be693aefae9546ae20b
Tanvi Garg <Tanvi@tanvis-mbp.lan>
2018-10-18 22:30:24 -0700

    recreate page with radio buttons and tweak jquery for radio button inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-$(document).ready(function() {
-  // $("form#submit").submit(function(event){
+//business-logic
+var inputOption;
+function checkAge(){
+  if (inputOption > 18) {
+    $("#over-18").show();
+    $("#under-18").hide();
+  } else if (inputOption === 18) {
+    alert("welcome first time voter!");
+    $("#over-18").hide();
+    $("#over-18").show();
+    $("#under-18").hide();
+  } else {
+    $("#under-18").show();
+    $("#over-18").hide();
+  }
+}
 
-  var age = parseInt(prompt("How old are you?"));
-    console.log(age);
-    if (age > 18) {
-      $("#over-18").show();
-    } else if (age === 18) {
-      alert("welcome first time voter!");
-      $("#over-18").show();
-    } else {
-      $("#under-18").show();
-    }
-  //   event.preventDefault();
-  // });
+//front-end logic
+$(document).ready(function() {
+  $("form#submit").submit(function(event){
+  inputOption = parseInt($("input:radio[name=radio-stacked]:checked").val());
+  console.log(inputOption);
+  checkAge(inputOption);
+  event.preventDefault();
+  });
 });
